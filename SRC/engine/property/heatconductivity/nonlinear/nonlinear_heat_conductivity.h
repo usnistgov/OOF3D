7876828cbb2f683a611b40df31404b4bc4d1b889
Steve Langer <stephen.langer@nist.gov>
2017-04-25 16:50:45 -0400

    Merge markers in this file somehow escaped.

--- SRC/engine/property/heatconductivity/nonlinear/nonlinear_heat_conductivity.h
+++ SRC/engine/property/heatconductivity/nonlinear/nonlinear_heat_conductivity.h
@@ -1,11 +1,4 @@
 // -*- C++ -*-
-<<<<<<< HEAD
-// $RCSfile: nonlinear_heat_conductivity.h,v $
-// $Revision: 1.13.8.2 $
-// $Author: langer $
-// $Date: 2013-11-08 20:45:49 $
-=======
->>>>>>> development
 
 /* This software was produced by NIST, an agency of the U.S. government,
  * and by statute is not subject to copyright in the United States.
